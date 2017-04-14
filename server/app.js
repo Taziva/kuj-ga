fc079cebecdfaa280b65042bc11efaef6236f435
Taziva <stmarovatsanga@gmail.com>
2017-04-14 17:09:20 +0100

    add route test

--- server/app.js
+++ server/app.js
@@ -1,22 +1,18 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./routes/routes');
 const app = express();
-const router = express.Router()
 
 //Set view engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'src', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'static')));
-app.use('/', router);
-
-router.get('/',function(req, res){
-  res.render('index', {
-    title: "Kuj-Ga"
-  })
-})
+app.use(express.static(path.join(__dirname, '..', 'src', 'static')));
+app.use('/', routes);
 
 var server = app.listen(3000, function(){
   var port = server.address().port
   console.log("All green server running on port: " + port);
 })
+
+module.exports = app
