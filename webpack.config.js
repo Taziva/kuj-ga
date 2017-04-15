aca6429f6c81bfe12aba83224b96f1bbf5a1ff66
Taziva <stmarovatsanga@gmail.com>
2017-04-15 19:16:19 +0100

    configured webpack with automation on server and output

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'app-client'),
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['','.js','.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:7700/src/static',
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    }],
+  },
+  plugins:[],
+};
