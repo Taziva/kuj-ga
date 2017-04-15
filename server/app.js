4d81fccc4610c393cc59ff63cd78727e1484170a
Taziva <stmarovatsanga@gmail.com>
2017-04-15 02:04:34 +0100

    add body-parser

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./routes/routes');
+var bodyParser = require('body-parser');
+require('dotenv').config()
 const app = express();
 
 //Set view engine
 app.set('views', path.join(__dirname, '..', 'src', 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'src', 'static')));
 app.use('/', routes);
 
