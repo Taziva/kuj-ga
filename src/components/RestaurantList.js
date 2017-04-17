95359ae5106b2ee53631a1912c7dc9957d794152
Taziva <stmarovatsanga@gmail.com>
2017-04-17 02:26:19 +0100

    adjusted css for class content

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FontIcon from 'material-ui/FontIcon';
 import Avatar from 'material-ui/Avatar'
 import FlatButton from 'material-ui/FlatButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -14,11 +15,20 @@ export default class RestaurantList extends React.Component {
     this.setText = this.setText.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
+  textStyle(){
+    return(
+      {textAlign: 'center',
+        color: '#66605a'}
+      )
+  }
   render(){
     return !this.props.items || this.props.items.error_message ?
     <Card className="content">
-      <CardText  style={{ textAlign: 'center' }}>
+      <CardText className="content-text" style={this.textStyle()}>
         Sorry, service currently not available
+      </CardText>
+      <CardText className="content-text" style={this.textStyle()}>
+        <FontIcon className="content-text" style={{fontSize: 192, color: '#66605a' }} className="fa fa-frown-o" aria-hidden="true"></FontIcon>
       </CardText>
     </Card>
     :
