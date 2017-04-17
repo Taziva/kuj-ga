51cf8a39a3f8f1ecc23f275e193852207ce8a25f
Taziva <stmarovatsanga@gmail.com>
2017-04-17 22:39:20 +0100

    catch location error

--- src/app-client.js
+++ src/app-client.js
@@ -43,6 +43,8 @@ $(document).ready(() => {
           </MuiThemeProvider>
         ), document.getElementById('content'));
       });
+    }).catch((err)=>{
+      alert("Sorry can't access location because:" err)
     })
   })
 });
